689be9b6a4538bb9864296bfce93831acac866d6
Enzo <enzopyton24@gmail.com>
2025-06-19 13:44:53 -0300

    add: função de adicionar itens na lista

---
+++ src/js/script2.js
@@ -0,0 +1,20 @@
+const botaoSalvar = document.getElementById("button")
+const textoInput = document.getElementById("input-texto")
+const listaDeCompras = document.querySelector("div")
+let contador = 0;
+
+botaoSalvar.addEventListener("click", (salvar)=>{
+    salvar.preventDefault()
+    if(textoInput.value === ""){
+        alert("digite algo antes de enviar")
+        return
+    }
+    console.log(textoInput.value)
+    
+    const nomeItem = document.createElement("p")
+    nomeItem.innerHTML = textoInput.value
+    const lista = document.createElement("li")
+    
+    lista.appendChild(nomeItem)
+    listaDeCompras.appendChild(lista)
+})
