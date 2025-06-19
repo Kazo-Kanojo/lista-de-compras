27b8e0ffa9d526e12f6a6abe49da50c9b2a9c324
Enzo <enzopyton24@gmail.com>
2025-06-19 01:17:13 -0300

    Adicionando funcionalidades usando o javascript

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
-const inputItem = document.getElementById("input-item")
+const inputItem = document.getElementById("input-item");
 
-console.log(inputItem.value)
+const botaoAdicionar = document.getElementById("adicionar-item")
+botaoAdicionar.addEventListener("click", (evento)=>{
+    evento.preventDefault();
+    if (inputItem.value === ''){
+        alert("preencha o campo para adicionar")
+    }else{
+        console.log(inputItem.value)
+    }
+
+
+
+
+    const itemDaLista = document.createElement("li")
+    const containerItemDaLista = document.createElement("div")
+    containerItemDaLista.classList.add("lista-item-conteiner")
+})
+
